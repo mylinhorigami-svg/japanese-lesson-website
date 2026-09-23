72de03c622972ab563105bc4a70fc1df6e6d717f
mylinhorigami-svg <mylinhorigami@gmail.com>
2026-09-23 10:09:01 +0700

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- pages/N5/grammar/lesson11/lesson11.js
+++ pages/N5/grammar/lesson11/lesson11.js
@@ -1368,7 +1368,9 @@ function createPracticeExampleHTML(example) {
                     ? `
                         <div class="grammar-practice-example-image">
                             <img
-                                src="${example.image}"
+                                src="${location.pathname.startsWith('/japanese-lesson-website/')
+    ? '/japanese-lesson-website' + example.image
+    : example.image}"
                                 alt="${example.imageAlt || ""}"
                             >
                         </div>
